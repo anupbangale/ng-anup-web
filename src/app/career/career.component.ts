4b17fe8e38d748a098c16996b58619570141035e
anup.bangale <anup.bangale@tectoro.com>
2019-09-11 18:36:13 +0530

    Elipsify in career component.
    Signed-off-by: anup.bangale <anup.bangale@tectoro.com>

--- src/app/career/career.component.ts
+++ src/app/career/career.component.ts
@@ -1,6 +1,5 @@
-import { OnInit, ChangeDetectionStrategy, } from "@angular/core";
-import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
-// import { DeprecatedI18NPipesModule } from '@angular/common';
+import { OnInit, ChangeDetectionStrategy, HostListener, } from "@angular/core";
+import { AfterViewInit, Component, ElementRef, ViewChild } from "@angular/core";
 
 @Component({
   selector: "app-career",
@@ -9,41 +8,42 @@ import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
   changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class CareerComponent implements OnInit, AfterViewInit {
-  @ViewChild('myDiv', { static: false }) myDiv: ElementRef;
+  @ViewChild("myDiv", { static: false }) myDiv: ElementRef;
 
   longText =
-    'Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,Lorem ipsum dolor sit amet,';
+    "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud.";
+
   constructor() { }
 
-  ngOnInit() {
-    console.log('ngOnInit');
+  ngOnInit() { }
+
+  @HostListener("window:resize", ["$event.target"])
+  checkEllipsify() {
+    const ele = this.myDiv.nativeElement;
+    // this.isEllipsisActive(ele);
+    if (ele.offsetWidth < ele.scrollWidth) {
+      console.log("if");
+      ele.setAttribute("title", ele.textContent);
+    } else {
+      console.log("else");
+      ele.removeAttribute("title");
+    }
   }
+
 
   ngAfterViewInit(): void {
-    console.log('ngAfterViewInit');
-    console.log('myDiv>>', this.myDiv);
-
+    console.log("ngAfterViewInit");
+    console.log("myDiv>>", this.myDiv);
     // console.log(this.myDiv.nativeElement.innerHTML);
     const ele = this.myDiv.nativeElement;
-    const flag = this.isEllipsisActive(ele);
-    console.log('flag>>', flag);
-
-  }
-
-
-
-  isEllipsisActive(e) {
-    console.log('>>', e);
-    console.log('e.offsetWidth', e.offsetWidth);
-    console.log('e.scrollWidth', e.scrollWidth);
-    console.log('...', e.innerHTML);
-    // e.childNodes[0].length
-    console.log('e.childNodes[0].length', e.childNodes[0].length);
-
-    // const length = e.childNodes[0].length
-    // el.offsetWidth < el.scrollWidth
-    return e.offsetWidth === e.scrollWidth
-    // return (e.offsetWidth > length);
+    // this.isEllipsisActive(ele);
+    if (ele.offsetWidth < ele.scrollWidth) {
+      console.log("if");
+      ele.setAttribute("title", ele.textContent);
+    } else {
+      console.log("else");
+      ele.removeAttribute("title");
+    }
   }
 
 }
